c34d375b9b83753effa462037bb773101d907500
IgorPerikov <perikov.igor@gmail.com>
2020-05-04 15:46:56 +0300

    tests for validation

--- src/validation.rs
+++ src/validation.rs
@@ -30,3 +30,55 @@ impl ValidationError {
         }
     }
 }
+
+#[cfg(test)]
+mod validation_tests {
+    use super::*;
+
+    const DIVISIBLE_DIMENSION: u32 = 10;
+    const NON_DIVISIBLE_DIMENSION: u32 = 11;
+    const TILE_SIZE: u32 = 2;
+
+    #[test]
+    fn perfectly_divisible_square_image_should_be_ok() {
+        let image = DynamicImage::new_rgb8(DIVISIBLE_DIMENSION, DIVISIBLE_DIMENSION);
+        let error_option = validate(&image, TILE_SIZE);
+        assert!(error_option.is_none());
+    }
+
+    #[test]
+    fn divisible_non_square_image_should_be_ok() {
+        let image = DynamicImage::new_rgb8(DIVISIBLE_DIMENSION, DIVISIBLE_DIMENSION * 2);
+        let error_option = validate(&image, TILE_SIZE);
+        assert!(error_option.is_none());
+    }
+
+    #[test]
+    fn non_divisible_by_width_image_should_return_error() {
+        let image = DynamicImage::new_rgb8(NON_DIVISIBLE_DIMENSION, DIVISIBLE_DIMENSION);
+        let error_option = validate(&image, TILE_SIZE);
+        assert!(error_option.is_some());
+    }
+
+    #[test]
+    fn non_divisible_by_height_image_should_return_error() {
+        let image = DynamicImage::new_rgb8(DIVISIBLE_DIMENSION, NON_DIVISIBLE_DIMENSION);
+        let error_option = validate(&image, TILE_SIZE);
+        assert!(error_option.is_some());
+    }
+
+    #[test]
+    fn non_divisible_by_width_and_height_image_should_return_error() {
+        let image = DynamicImage::new_rgb8(NON_DIVISIBLE_DIMENSION, NON_DIVISIBLE_DIMENSION);
+        let error_option = validate(&image, TILE_SIZE);
+        assert!(error_option.is_some());
+    }
+
+    #[test]
+    fn calling_panic_on_validation_error_should_panic() {
+        let result = std::panic::catch_unwind(|| {
+            ValidationError { error: String::from("error message") }.panic();
+        });
+        assert!(result.is_err());
+    }
+}
