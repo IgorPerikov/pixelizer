aac7427bffdccee828a7928c247c1c26b048820a
IgorPerikov <perikov.igor@gmail.com>
2020-05-04 11:57:23 +0300

    encapsulate iteration logic inside ImageSegmentIterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
+use std::ops::Range;
 
 // TODO: tests
 
@@ -17,51 +18,44 @@ pub fn pixelize(input: &mut DynamicImage, tile_size: u32) -> Result<(), Validati
 fn pixelize_no_validation(input: &mut DynamicImage, tile_size: u32) {
     for row_segment_number in 0..(input.height() / tile_size) {
         for column_segment_number in 0..(input.width() / tile_size) {
-            pixelize_segment(input, row_segment_number, column_segment_number, tile_size);
+            let image_segment_iterator = ImageSegmentIterator {
+                row_segment_number,
+                column_segment_number,
+                segment_size: tile_size,
+            };
+            pixelize_segment(input, &image_segment_iterator);
         }
     }
 }
 
-fn pixelize_segment(
-    input: &mut DynamicImage,
-    row_segment_number: u32,
-    column_segment_number: u32,
-    tile_size: u32,
-) {
-    let start_row = row_segment_number * tile_size;
-    let start_column = column_segment_number * tile_size;
-
-    let rgba = calc_average_rgba_pixel(input, start_row, start_column, tile_size);
-
-    for row in start_row..(start_row + tile_size) {
-        for column in start_column..(start_column + tile_size) {
-            input.put_pixel(row, column, rgba);
-        }
+// TODO: can be launched in parallel
+fn pixelize_segment(input: &mut DynamicImage, image_segment_iterator: &ImageSegmentIterator) {
+    let rgba = calc_average_rgba_pixel(input, image_segment_iterator);
+    for (row, column) in image_segment_iterator.get_elements() {
+        input.put_pixel(row, column, rgba);
     }
 }
 
 fn calc_average_rgba_pixel(
     input: &mut DynamicImage,
-    start_row: u32,
-    start_column: u32,
-    tile_size: u32,
+    image_segment_iterator: &ImageSegmentIterator,
 ) -> Rgba<u8> {
     let mut sum_red: u64 = 0;
     let mut sum_green: u64 = 0;
     let mut sum_blue: u64 = 0;
     let mut sum_alpha: u64 = 0;
 
-    for row in start_row..(start_row + tile_size) {
-        for column in start_column..(start_column + tile_size) {
-            let rgba_content = input.get_pixel(row, column).0;
-            sum_red += rgba_content[0] as u64;
-            sum_green += rgba_content[1] as u64;
-            sum_blue += rgba_content[2] as u64;
-            sum_alpha += rgba_content[3] as u64;
-        }
+    let elements = image_segment_iterator.get_elements();
+
+    for (row, column) in &elements {
+        let rgba_content = input.get_pixel(*row, *column).0;
+        sum_red += rgba_content[0] as u64;
+        sum_green += rgba_content[1] as u64;
+        sum_blue += rgba_content[2] as u64;
+        sum_alpha += rgba_content[3] as u64;
     }
 
-    let total_pixels: u64 = tile_size.pow(2).into();
+    let total_pixels = elements.len() as u64;
 
     return Rgba([
         (sum_red / total_pixels) as u8,
@@ -69,6 +63,35 @@ fn calc_average_rgba_pixel(
         (sum_blue / total_pixels) as u8,
         (sum_alpha / total_pixels) as u8,
     ]);
+}
+
+// TODO: encapsulate get_columns/get_rows/struct fields by moving out of lib.rs
+struct ImageSegmentIterator {
+    row_segment_number: u32,
+    column_segment_number: u32,
+    segment_size: u32,
+}
+
+impl ImageSegmentIterator {
+    fn get_elements(&self) -> Vec<(u32, u32)> {
+        let mut elements = Vec::new();
+        for row in self.get_rows() {
+            for column in self.get_columns() {
+                elements.push((row, column));
+            }
+        }
+        return elements;
+    }
+
+    fn get_columns(&self) -> Range<u32> {
+        let start_column = self.column_segment_number * self.segment_size;
+        return start_column..(start_column + self.segment_size);
+    }
+
+    fn get_rows(&self) -> Range<u32> {
+        let start_row = self.row_segment_number * self.segment_size;
+        return start_row..(start_row + self.segment_size);
+    }
 }
 
 #[derive(Debug)]
@@ -81,9 +104,7 @@ impl ValidationError {
         ValidationError {
             error: format!(
                 "{}x{} image cannot be segmented into tiles of size {}",
-                width,
-                height,
-                tile_size
+                width, height, tile_size
             ),
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use image::GenericImageView;
 use pixelizer::pixelize;
 
 // TODO: support cli parameters
 // TODO: consider pixelizing even if it's not perfectly divisible
 fn main() {
     let mut input_image = image::open("avatar.jpeg").unwrap();
-    let tile_size = 5;
+    let tile_size = 20;
     match pixelize(&mut input_image, tile_size) {
         Err(e) => e.panic(),
         _ => {}
